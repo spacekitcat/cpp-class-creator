793169b6d3eda23b8dcc2e9db4db5da2869715d2
Lisa Burton <spacekitcat@protonmail.com>
2021-09-05 14:32:18 +0100

    feat: customisable file and class naming schemes, independent of each other. Ran formatter

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 
-async function create_name_input()
-{
+async function create_name_input() {
 	var option: vscode.InputBoxOptions = {
 		ignoreFocusOut: false,
 		placeHolder: "foo it in the bar.",
-		prompt: "Type your class name"
+		prompt: "Type your class name in PascalCase"
 	};
 	return vscode.window.showInputBox(option);
 }
 
-async function create_path_input()
-{
+async function create_path_input() {
 	var option: vscode.InputBoxOptions = {
 		ignoreFocusOut: false,
 		placeHolder: "Give me your path.",
@@ -25,80 +23,70 @@ async function create_path_input()
 	return await vscode.window.showInputBox(option);
 }
 
-function create_hpp_buffer(name: string)
-{
+function create_hpp_buffer(name: string) {
 
 
-	const ifndef_head = 
-	`#ifndef `+name.toUpperCase()+`_H
-#define `+name.toUpperCase()+`_H`;
+	const ifndef_head =
+		`#ifndef ` + name.toUpperCase() + `_H
+#define `+ name.toUpperCase() + `_H`;
 
 
 	const pragma_once_buffer =
-	`
+		`
 #pragma once`;
 
 	const default_info = `
 	
-class ` + name +`  
+class ` + name + `  
 {
 	private:
 
 	public:
 
-		`+ name +`();
-		~`+name+`();
+		`+ name + `();
+		~`+ name + `();
 
 };`;
 
-	const ifndef_end= `
+	const ifndef_end = `
 #endif`;
 
-	var output : string;
-	const useIfnDef : boolean = vscode.workspace.getConfiguration().get("cpp.creator.headerProtection.useIfnDef") as boolean;
-	const usePragma : boolean = vscode.workspace.getConfiguration().get("cpp.creator.headerProtection.usePragmaOnce") as boolean;
+	var output: string;
+	const useIfnDef: boolean = vscode.workspace.getConfiguration().get("cpp.creator.headerProtection.useIfnDef") as boolean;
+	const usePragma: boolean = vscode.workspace.getConfiguration().get("cpp.creator.headerProtection.usePragmaOnce") as boolean;
 
-	if(useIfnDef && usePragma)
-	{
-		output = ifndef_head+pragma_once_buffer+default_info+ifndef_end;
+	if (useIfnDef && usePragma) {
+		output = ifndef_head + pragma_once_buffer + default_info + ifndef_end;
 	}
-	else if(useIfnDef)
-	{
-		output = ifndef_head+default_info+ifndef_end;		
+	else if (useIfnDef) {
+		output = ifndef_head + default_info + ifndef_end;
 	}
-	else if(usePragma)
-	{
-		output = pragma_once_buffer+default_info;
+	else if (usePragma) {
+		output = pragma_once_buffer + default_info;
 	}
-	else
-	{
+	else {
 		output = default_info;
 	}
 
 	return output;
 }
 
-function get_include_name(name: string)
-{
+function get_include_name(name: string) {
 	var include_name: string;
-	if (vscode.workspace.getConfiguration().get("cpp.creator.useHPPEnding") as boolean === true)
-	{
+	if (vscode.workspace.getConfiguration().get("cpp.creator.useHPPEnding") as boolean === true) {
 		include_name = name + '.hpp';
 	}
-	else 
-	{
+	else {
 		include_name = name + '.h';
 	}
-	
+
 	return include_name;
 }
 
-function create_hpp(className: string, fileName: string, dir: string)
-{
+function create_hpp(className: string, fileName: string, dir: string) {
 	var hpp_buffer = create_hpp_buffer(className);
-	var hpp_name = dir+"/"+get_include_name(fileName);
-	fs.writeFile(hpp_name, hpp_buffer, function (err)
-	{
+	var hpp_name = dir + "/" + get_include_name(fileName);
+	fs.writeFile(hpp_name, hpp_buffer, function (err) {
 		if (err) {
 			console.error(err);
 			return false;
@@ -109,18 +97,17 @@ function create_hpp(className: string, fileName: string, dir: string)
 	return true;
 }
 
-function create_cpp_buffer(className: string, fileName: string)
-{
+function create_cpp_buffer(className: string, fileName: string) {
 	var hpp_name = get_include_name(fileName);
 	var cpp_buffer =
-	`#include "` + hpp_name + `"  
+		`#include "` + hpp_name + `"  
 	
-`+className+`::`+ className +`()
+`+ className + `::` + className + `()
 {
 	
 }
 	
-`+className+`::~`+ className + `()
+`+ className + `::~` + className + `()
 {
 	
 }`;
@@ -128,12 +115,10 @@ function create_cpp_buffer(className: string, fileName: string)
 	return cpp_buffer;
 }
 
-function create_cpp(className: string, fileName: string, dir: string)
-{
+function create_cpp(className: string, fileName: string, dir: string) {
 	var cpp_buffer = create_cpp_buffer(className, fileName);
-	var cpp_name = dir+"/"+fileName + '.cpp';
-	fs.writeFile(cpp_name, cpp_buffer, function (err)
-	{
+	var cpp_name = dir + "/" + fileName + '.cpp';
+	fs.writeFile(cpp_name, cpp_buffer, function (err) {
 		if (err) {
 			console.error(err);
 			return false;
@@ -143,8 +128,7 @@ function create_cpp(className: string, fileName: string, dir: string)
 	return true;
 }
 
-function create_class(className: string, fileName: string, dir: string)
-{
+function create_class(className: string, fileName: string, dir: string) {
 	if (fs.existsSync(dir)) {
 		var stats = fs.lstatSync(dir);
 
@@ -161,28 +145,23 @@ function create_class(className: string, fileName: string, dir: string)
 	return (hpp && cpp);
 }
 
-function can_continue(res: any)
-{
-	if (!res)
-	{
+function can_continue(res: any) {
+	if (!res) {
 		vscode.window.showErrorMessage("Your Class could not be created!");
 		return false;
 	}
-	else if (res.length > 60)
-	{
+	else if (res.length > 60) {
 		vscode.window.showErrorMessage("Class name to long!");
 		return false;
 	}
-	else if (res.indexOf(' ') >= 0)
-	{
+	else if (res.indexOf(' ') >= 0) {
 		vscode.window.showErrorMessage("Class name should not have spaces!");
 		return false;
 	}
 	return true;
 }
 
-function format_kebab_case(name: String): String
-{
+function format_kebab_case(name: String): string {
 	return name.replace(/[A-Z]+/g, "-$&").slice(1).toLowerCase();
 }
 
@@ -197,54 +176,58 @@ export async function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.createClass', async (args) => {
 		// The code you place here will be executed every time your command is executed
 
-		var className = await create_name_input();
-			if(!can_continue(className)) return; // check for class name
+		var classNameInput = await create_name_input();
 
-			let dir :string | undefined | boolean= vscode.workspace.getConfiguration().get("cpp.creator.setPath");
-			// If it's called via the context menu, it's gonna have the _fsPath set from where you're clicking
-			if (args != null && args._fsPath != null) {
-				dir = args._fsPath;
-				if (typeof dir === "string" && fs.existsSync(dir)) {
-					var stats = fs.lstatSync(dir);
-					if (!stats.isDirectory()) {
-						//If it's not a directory then it's the file so get the parent directory
-						dir = path.dirname(args._fsPath);
-					}
+		if (!can_continue(classNameInput)) return; // check for class name
+
+		// TODO: A better dispatch mechanism would make it easier to add additional conventions
+		var className = classNameInput as string;
+		if (vscode.workspace.getConfiguration().get("cpp.creator.class.namingScheme") === 'kebab-case') {
+			className = format_kebab_case(className);
+		}
+		let fileName = classNameInput as String
+		if (vscode.workspace.getConfiguration().get("cpp.creator.file.namingScheme") === 'kebab-case') {
+			fileName = format_kebab_case(fileName);
+		}
+
+		let dir: string | undefined | boolean = vscode.workspace.getConfiguration().get("cpp.creator.setPath");
+		// If it's called via the context menu, it's gonna have the _fsPath set from where you're clicking
+		if (args != null && args._fsPath != null) {
+			dir = args._fsPath;
+			if (typeof dir === "string" && fs.existsSync(dir)) {
+				var stats = fs.lstatSync(dir);
+				if (!stats.isDirectory()) {
+					//If it's not a directory then it's the file so get the parent directory
+					dir = path.dirname(args._fsPath);
 				}
 			}
-			if (dir == null || dir == false) {
-				dir = vscode.workspace.rootPath as string; // use workspace path
-				let createFolder: boolean | undefined = vscode.workspace.getConfiguration().get("cpp.creator.createFolder");
-				if (createFolder) // create the folder where to put the class
-					dir += "/" + className;
+		}
+		if (dir == null || dir == false) {
+			dir = vscode.workspace.rootPath as string; // use workspace path
+			let createFolder: boolean | undefined = vscode.workspace.getConfiguration().get("cpp.creator.createFolder");
+			if (createFolder) // create the folder where to put the class
+				dir += "/" + className;
+		}
+		else if (dir == true) {
+			dir = await create_path_input(); // ask for path
+			if (!dir) {
+				dir = vscode.workspace.rootPath as string; // if empty input, just use workspace path
 			}
-			else if (dir == true)
-			{
-				dir = await create_path_input(); // ask for path
-				if (!dir)
-				{
-					dir = vscode.workspace.rootPath as string; // if empty input, just use workspace path
-				}
-			}
-			let fileName = className as String
-			if (vscode.workspace.getConfiguration().get("cpp.creator.file.namingScheme") === 'KebabCase') {
-				fileName = format_kebab_case(fileName);
-			}
-			var out = create_class(className as string, fileName as string, dir as string); // if setPath was neither false, null or true, it was a string, so maybe a valid path? 
-																  //Create the class there
-			if (out)
-			{
-					vscode.window.showInformationMessage('Your Class ' + className + '  has been created!');
-			}
-			else
-			{
-				vscode.window.showErrorMessage('Your Class ' + className + '  has been not created!');
-			}
-		});
-		// Display a message box to the user
+		}
+
+		var out = create_class(className as string, fileName as string, dir as string); // if setPath was neither false, null or true, it was a string, so maybe a valid path? 
+		//Create the class there
+		if (out) {
+			vscode.window.showInformationMessage('Your Class ' + className + '  has been created!');
+		}
+		else {
+			vscode.window.showErrorMessage('Your Class ' + className + '  has been not created!');
+		}
+	});
+	// Display a message box to the user
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
